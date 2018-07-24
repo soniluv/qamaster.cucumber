0086829da7d5d2683834dfdb0afe898bdf8e4ad6
soniluv <39604627+soniluv@users.noreply.github.com>
2018-07-24 11:51:49 +0100

    Update RunTests.java

--- src/test/java/RunTests.java
+++ src/test/java/RunTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
  * Created by madduris on 29/06/2018.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/automationPractice/features/",
+@CucumberOptions(features = "src/test/java/automationPractice/features/",plugin = "json:target/cucumberreport.json",
         tags = {"@validLogin,@basket"})
 public class RunTests {
 
